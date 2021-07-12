de5b9d5029ae32fc85e540ac98a3cfb646ed80b5
Максим Снегирев <35629955+Drygok@users.noreply.github.com>
2021-07-13 02:59:58 +0400

    Init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Calcuum  
+Calcuum is a calculator. Yes, just a calculator in C#.  
+In fact, they just threw me a funny TikTok about a programmer, after which I remembered that I had never made calculators. And so, I did.  
+  
+And here is the video itself, take a look:  https://youtu.be/HFLq5sN87qQ  
+I added subtitles ;-)  
---
+++ src/Calcuum/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calcuum
+{
+    class Program
+    {
+        private static readonly Dictionary<string, Func<double, double, double>> operations = new Dictionary<string, Func<double, double, double>>()
+        {
+            {
+                "+",
+                ((double x, double y) =>
+                {
+                    return x + y;
+                })
+            },
+            {
+                "-",
+                ((double x, double y) =>
+                {
+                    return x - y;
+                })
+            },
+            {
+                "*",
+                ((double x, double y) =>
+                {
+                    return x * y;
+                })
+            },
+            {
+                "/",
+                ((double x, double y) =>
+                {
+                    return x / y;
+                })
+            }
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, о дивный новый мир");
+            Console.WriteLine("Чтобы использовать возможности калькулятора, Вы можете просто печатать выражение в поле ввода\n");
+            Console.WriteLine("Для закрытия приложения введите команду exit");
+
+            string input = "";
+            while (input != "exit")
+            {
+                string[] commands = input.Split(' ');
+                if (commands.Length == 3)
+                {
+                    if (operations.ContainsKey(commands[1]))
+                    {
+                        try
+                        {
+                            Console.WriteLine("Результат операции: " + operations[commands[1]].Invoke(Double.Parse(commands[0].Replace('.', ',')), Double.Parse(commands[2].Replace('.', ','))));
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Аргументы не опознаны: Пожалуйста, повторите ввод");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Операция не опознана. Допустимые операции:");
+                        foreach (string operation in operations.Keys)
+                        {
+                            Console.Write("\t" + operation);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    if (input != "")
+                    {
+                        Console.WriteLine("Действие не распознано");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.Write("Введите выражение, разделяя каждый его элемент пробелами (например, \"2 + 2\"): ");
+                input = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
